a16380250d0966ed28fed1296086f1668178f0b2
daradanilova <lemma1102@gmail.com>
2024-11-15 14:20:18 +0200

    add task solution

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { Todo } from '../../types/Todo';
+import { TodoItem } from '../TodoItem/TodoItem';
+
+type Props = {
+  todos: Todo[];
+  tempTodo: Todo | null;
+  loading: number[];
+  onDeleteTodo: (value: number) => Promise<void>;
+  onUpdateTodo: (updatedTodo: Todo) => Promise<void>;
+};
+
+export const TodoList: React.FC<Props> = ({
+  todos,
+  tempTodo,
+  loading,
+  onDeleteTodo,
+  onUpdateTodo,
+}) => {
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      <TransitionGroup>
+        <div className="todoapp__main" data-cy="TodoList">
+          {todos.map(todo => (
+            <CSSTransition key={todo.id} timeout={300} classNames="item">
+              <TodoItem
+                todo={todo}
+                // key={todo.id}
+                onDeleteTodo={onDeleteTodo}
+                isLoading={loading.includes(todo.id)}
+                onUpdateTodo={onUpdateTodo}
+              />
+            </CSSTransition>
+          ))}
+
+          {tempTodo && (
+            <CSSTransition key={0} timeout={300} classNames="temp-item">
+              <TodoItem todo={tempTodo} isProcessed />
+            </CSSTransition>
+          )}
+        </div>
+      </TransitionGroup>
+    </section>
+  );
+};
